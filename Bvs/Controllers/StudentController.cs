d00f31283a5713d32474e2070b385314d1530c7a
AhmadAlberkawi <46059053+AhmadAlberkawi@users.noreply.github.com>
2021-11-19 19:53:02 +0100

    Projektdateien hinzufügen.

---
+++ Bvs/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using Bvs.Entities;
+using Bvs_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bvs_API.Controllers
+{   
+    [ApiController]
+    [Route("Bvs_Api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public StudentController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            return await _context.Student.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            return await _context.Student.FindAsync(id);
+        }
+    }
+}
---
+++ Bvs/Entities/Borrow.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bvs.Entities
+{
+    [Keyless]
+    public class Borrow
+    {
+        public Student students { get; set; }
+        public Book Books { get; set; }
+    }
+}
