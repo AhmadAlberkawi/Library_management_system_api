8ff46bb7d1582c24a1307788783afc63547a077a
AhmadAlberkawi <46059053+AhmadAlberkawi@users.noreply.github.com>
2022-01-04 15:00:54 +0100

    Admin Profil edit and change Password

--- Bvs/Controllers/AdminController.cs
+++ Bvs/Controllers/AdminController.cs
@@ -14,7 +14,7 @@ using System.Threading.Tasks;
 
 namespace Bvs_API.Controllers
 {
-    public class AdminController: BaseApiController
+    public class AdminController : BaseApiController
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
@@ -25,10 +25,18 @@ namespace Bvs_API.Controllers
             _tokenService = tokenService;
         }
 
+        //[Authorize]
+        [HttpGet]
+        public async Task<IEnumerable<AdminDto>> GetAdmins()
+        {
+            return await _context.Admin
+                .Select(x => new AdminDto(x.Id, x.Name, x.Vorname, x.Email, x.Foto, x.Rolle)).ToListAsync();
+        }
+
         [HttpPost("addAdmin")]
         public async Task<ActionResult<AdminTokenDto>> AddAdmin(AdminRegisterDto adminDto)
         {
-            if(await AdminNameExists(adminDto.Username))
+            if (await AdminNameExists(adminDto.Username))
             {
                 return BadRequest("Admin username is taken.");
             }
@@ -54,7 +62,7 @@ namespace Bvs_API.Controllers
 
             _context.Admin.Add(admin);
             await _context.SaveChangesAsync();
-            
+
             await AdminsCount();
             await _context.SaveChangesAsync();
 
@@ -70,42 +78,39 @@ namespace Bvs_API.Controllers
             };
         }
 
-        private async Task<bool> AdminNameExists(string username)
-        {
-            return await _context.Admin.AnyAsync(x => x.Username.ToLower() == username.ToLower());
-        }
-
-        private async Task<bool> AdminEmailExists(string email)
-        {
-            return await _context.Admin.AnyAsync(x => x.Email.ToLower() == email.ToLower());
-        }
-        
         [HttpPost("login")]
         public async Task<ActionResult<AdminTokenDto>> LoginAdmin(AdminLoginDto adminLoginDto)
         {
             var admin = await _context.Admin.SingleOrDefaultAsync(x => x.Username == adminLoginDto.UsernameOrEmail);
 
-            if(admin == null)
+            if (admin == null)
             {
                 admin = await _context.Admin.SingleOrDefaultAsync(x => x.Email == adminLoginDto.UsernameOrEmail);
             }
-                
-            if(admin == null)
+
+            if (admin == null)
             {
                 return Unauthorized("Username or email is invalid.");
             }
 
-            using var hmac = new HMACSHA512(admin.PasswordSalt);
+            bool password = CheckPassword(admin.PasswordSalt, admin.PasswordHash, adminLoginDto.Password);
 
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(adminLoginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
+            if (!password)
             {
-                if (computedHash[i] != admin.PasswordHash[i])
-                { 
-                    return Unauthorized("Password is invalid.");
-                }
+                return Unauthorized("Password is invalid.");
             }
+
+            //using var hmac = new HMACSHA512(admin.PasswordSalt);
+
+            //var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(adminLoginDto.Password));
+
+            //for (int i = 0; i < computedHash.Length; i++)
+            //{
+            //    if (computedHash[i] != admin.PasswordHash[i])
+            //    { 
+            //        return Unauthorized("Password is invalid.");
+            //    }
+            //}
 
             return new AdminTokenDto
             {
@@ -119,12 +124,71 @@ namespace Bvs_API.Controllers
             };
         }
 
-        //[Authorize]
-        [HttpGet]
-        public async Task<IEnumerable<AdminDto>> GetAdmins()
+        [HttpPut("editAdmin")]
+        public async Task<ActionResult> EditProfil(AdminRegisterDto adminEdit)
         {
-            return await _context.Admin
-                .Select(x => new AdminDto(x.Id, x.Name, x.Vorname, x.Email, x.Foto, x.Rolle)).ToListAsync();
+            Admin admin = await _context.Admin.SingleOrDefaultAsync(x => x.Email == adminEdit.Email);
+
+            if (admin != null)
+            {
+                bool password = CheckPassword(admin.PasswordSalt, admin.PasswordHash, adminEdit.Password);
+
+                if (!password)
+                {
+                    return Unauthorized("Password is invalid.");
+                }
+
+                if (await AdminNameExists(adminEdit.Username))
+                {
+                    return BadRequest("Admin username is taken.");
+                }
+
+                admin.Username = adminEdit.Username;
+                admin.Vorname = adminEdit.Vorname;
+                admin.Name = adminEdit.Name;
+                //  admin.Foto = ""; //adminEdit.Foto;
+
+                await _context.SaveChangesAsync();
+
+                return Accepted("Aenderungen gespeichert");
+            }
+            else
+            {
+                return NotFound($"Admin mit Emailadresse: {adminEdit.Email} wurde nicht gefunden.");
+            }
+        }
+
+        [HttpPut("changePassword")]
+        public async Task<ActionResult> ChangePassword(AdminPasswordDto adminPassword)
+        {
+            Admin admin = await _context.Admin.SingleOrDefaultAsync(x => x.Email == adminPassword.Email);
+
+            if (admin != null)
+            {
+                // check the old password
+
+                bool password = CheckPassword(admin.PasswordSalt, admin.PasswordHash, adminPassword.Password);
+
+                if (!password)
+                {
+                    return Unauthorized("Password is invalid.");
+                }
+
+                // change password to new 
+
+                using var hmac = new HMACSHA512();
+
+                admin.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(adminPassword.NewPassword));
+                admin.PasswordSalt = hmac.Key;
+
+                await _context.SaveChangesAsync();
+
+                return Accepted("Password Changed");
+            }
+            else
+            {
+                return NotFound($"Admin mit Emailadresse: {adminPassword.Email} wurde nicht gefunden.");
+            }
         }
 
         [HttpDelete("{id:int}")]
@@ -132,7 +196,7 @@ namespace Bvs_API.Controllers
         {
             var admin = await _context.Admin.FindAsync(id);
 
-            if(admin != null)
+            if (admin != null)
             {
                 _context.Admin.Remove(admin);
                 await _context.SaveChangesAsync();
@@ -144,8 +208,42 @@ namespace Bvs_API.Controllers
             else
             {
                 return NotFound($"Admin with Id= {id} not found");
-            } 
+            }
         }
+
+        // Check methods for element exsit 
+
+        private async Task<bool> AdminNameExists(string username)
+        {
+            return await _context.Admin.AnyAsync(x => x.Username.ToLower() == username.ToLower());
+        }
+
+        private async Task<bool> AdminEmailExists(string email)
+        {
+            return await _context.Admin.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+        }
+
+        // Check Password
+
+        private bool CheckPassword(byte[] PasswordSaltFromDB, byte[] PasswordHashFromDB,
+            string PasswordFromClient)
+        {
+            using var hmac = new HMACSHA512(PasswordSaltFromDB);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(PasswordFromClient));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != PasswordHashFromDB[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Admin counter for overview Page
 
         private async Task AdminsCount()
         {
